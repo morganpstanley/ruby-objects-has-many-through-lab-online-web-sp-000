a1816693b7c994abc2b694ffc5d107b128615f5c
Morgan Stanley <morganpstanley@gmail.com>
2019-07-17 05:57:11 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -10,7 +10,7 @@ class Doctor
     @@all << self
   end
 
-  def new_appointment(date, patient)
+  def new_appointment(patient, date)
     @date = date
     @patient = patient
     self.patients << patient
