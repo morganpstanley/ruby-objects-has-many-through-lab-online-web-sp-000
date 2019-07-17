78a7fe0c7efa7c23ef42c911f856e5ef5e152519
Morgan Stanley <morganpstanley@gmail.com>
2019-07-17 05:28:58 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -2,10 +2,13 @@ class Doctor
 
   def initialize(name)
     @name = name
+    @appointments = []
   end
 
   def new_appointment(date, patient)
     @date = date
     @patient = patient
     @patient = Appointment.new(date, patient, self)
+
+
 end
